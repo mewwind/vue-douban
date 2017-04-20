6a086f1fb17a9f742e0b43183b5639eb6d76320a
mewwind <mewwind@163.com>
2017-04-20 08:59:29 +0800

    update some css

--- src/view/movie/Tab.vue
+++ src/view/movie/Tab.vue
@@ -2,18 +2,22 @@
 	<div id="tab">
 		<mu-drawer class="drawer" :open="open" :docked="docked" @close="toggle()">
 			<div class="drawer-pic">
-				<div class="title" style="font-size: 32px;">Web Doub</div>
-				<div class="description" style="font-size: 16px;color:#e0e0e0;">一个移动端豆瓣 Web 应用</div>
+				<div class="title" style="font-size: 28px;">Web Douban</div>
+				<div class="description" style="font-size: 16px;color:white;">一个移动端豆瓣 Web 应用</div>
 			</div>
-			<mu-list value="movie" @itemClick="docked ? '' : toggle()" @change="change">
-				<mu-list-item value="movie" title="电影"/>
-					<mu-icon value="movie"/>
+			<mu-list :value="value" @itemClick="docked ? '' : toggle()" @change="change">
+				<mu-list-item :value="movie" title="电影">
+					<mu-icon slot="left" value="movie"/>
 				</mu-list-item>
-				<mu-list-item value="book" title="图书"/>
-					<mu-icon value="book"/>
+				<mu-list-item :value="book" title="图书">
+					<mu-icon slot="left" value="book"/>
 				</mu-list-item>
-				<mu-list-item value="music" title="音乐"/>
-					<mu-icon value="music_note"/>
+				<mu-list-item :value="music" title="音乐">
+					<mu-icon slot="left" value="music_note"/>
+				</mu-list-item>
+				<mu-divider shallowInset/>
+				<mu-list-item :value="about" title="关于">
+					<mu-icon slot='left' value="info" />
 				</mu-list-item>
 	      	</mu-list>
 		</mu-drawer>
@@ -55,13 +59,9 @@
     background:#f5f5f5;
 }
 .drawer-pic{
-	background: gray;
-	padding: 70px;
+	background: #4fbfb5;
+	padding: 70px 0;
 	text-align: center;
 	color: white;
-}
-.mu-icon {
-  margin-right: 12px;
-  margin-bottom: 12px;
 }
 </style>
