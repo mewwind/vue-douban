f8ce545cfb66e94413f24b638c543fdf548524b2
mewwind <mewwind@163.com>
2017-04-22 10:15:21 +0800

    update About page

--- src/view/About/About.vue
+++ src/view/About/About.vue
@@ -1,22 +1,47 @@
 <template>
-	<div>
-		<mu-appbar title="关于">
-			<mu-icon-button icon="arrow_back" slot="left" @click="back"/>
-		</mu-appbar>
-		<description></description>
-    <mu-divider />
-    <mu-paper>
-    	
-    </mu-paper>
-	</div>
+  <div>
+    <mu-appbar title="关于">
+      <mu-icon-button icon="arrow_back" slot="left" @click="back"/>
+    </mu-appbar>
+    <mu-card>
+      <mu-card-media>
+        <img src="/static/img/action.jpg" />
+      </mu-card-media>
+      <mu-card-title title="Web Douban App" subTitle="movie,book,music..."/>
+      <mu-card-text>
+        This is a demo for douban app with vuejs, webpack.
+        It gives you a brief description for movie, books and music.
+        For more code infomation, please refer to the following github.
+      </mu-card-text>
+      <mu-card-actions>
+        <a href="https://github.com/mewwind/vue-douban/" target="_Blank">
+          <img class="github" src="/static/img/github.png" />
+        </a>
+      </mu-card-actions>
+    </mu-card>
+    <mu-divider /> 
+
+  </div>
 </template>
 <script>
-	import description from "../../components/Description"
-	export default{
-		name: 'About',
-		components: {description},
-	}
+  import description from "../../components/Description"
+  export default{
+    name: 'About',
+    components: {description},
+    methods: {
+      back() {
+        this.$router.go(-1);
+      }
+    }
+  }
 </script>
 <style lang="css">
-	
+  .action{
+    width:100%;
+  }
+  .github{
+    display:block;
+    margin:auto;
+    width:25px;
+  }
 </style>
