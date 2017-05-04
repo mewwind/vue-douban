20e5b590e3ae7cb8fb0658b36aefdf0c024541b4
Mill Zhou <mewwind@163.com>
2017-05-04 13:12:17 +0800

    Use flex layout for image and description

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -2,20 +2,21 @@
 	<div id="card">
     <router-link :to="{name: 'MovieSubject', params: {id: subject.id}}">
       <mu-paper>
-          <div class="movie-image" :style="'background-image:url('+subject.images.large+')'"></div>
-          <div class="description">
-            <div class="title">
-              <span class="name">{{subject.title}}</span>
-              <span class="year">{{subject.year}}</span>
-            </div>
-            <div class="star-rating" v-if="subject.rating.average>0">
-              <star-rating :size="13" :stars="subject.rating.stars" :rating="subject.rating.average" :max="subject.rating.max"></star-rating>
-            </div>
-            <div v-else>暂无评分</div>
-            <div>类型：{{subject.genres | arr2string}}</div>
-            <div>导演：{{subject.directors.map(e=>e.name) | arr2string}}</div>
-            <div>主演：{{subject.casts.map(e=>e.name) | arr2string}}</div>
+        <div class="card-image" :style="'background-image:url('+subject.images.large + ')'">
+        </div>
+        <div class="description">
+          <div class="title">
+            <span class="name">{{subject.title}}</span>
+            <span class="year">{{subject.year}}</span>
           </div>
+          <div class="star-rating" v-if="subject.rating.average>0">
+            <star-rating :size="13" :stars="subject.rating.stars" :rating="subject.rating.average" :max="subject.rating.max"></star-rating>
+          </div>
+          <div v-else>暂无评分</div>
+          <div>类型：{{subject.genres | arr2string}}</div>
+          <div>导演：{{subject.directors.map(e=>e.name) | arr2string}}</div>
+          <div>主演：{{subject.casts.map(e=>e.name) | arr2string}}</div>
+        </div>
       </mu-paper>
     </router-link>
   </div>
@@ -33,18 +34,16 @@
   #card {
     .mu-paper {
       margin:10px;
-    }
-    .movie-image {
-      width: 30%;
-      background-size: cover;
-      padding-top:42%;
-      display:inline-block;
-    }
-    .description {
-      width:70%;
-      display:inline-block;
-      position:absolute;
-      padding:10px;
+      display:flex;
+      .card-image {
+        flex:1;
+        background-size:cover;
+        width:1px;
+      }
+      .description {
+        flex:2;
+        padding:10px;
+      }
     }
   }
 </style>
