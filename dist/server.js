cb8ded7d425f8a495df5da3ff9340e6ec93a36a5
mewwind <mewwind@163.com>
2017-05-06 23:38:13 +0800

    Add http proxy for douban api on heroku

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var path = require("path");
+var proxy = require('express-http-proxy');
 
 app = express();
 app.use(express.static(__dirname));
+app.use('/api', proxy('http://api.douban.com/v2'));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
